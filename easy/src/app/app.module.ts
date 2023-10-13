9f7bef9b28c38b9d6c44b32f56fed9db51edfcd1
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-10-12 22:55:32 -0500

    creating a Javascript repo update!

---
+++ easy/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EasyComponent } from './easy/easy.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EasyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ easy/src/app/easy/easy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-easy',
+  templateUrl: './easy.component.html',
+  styleUrls: ['./easy.component.css']
+})
+export class EasyComponent {
+
+}
