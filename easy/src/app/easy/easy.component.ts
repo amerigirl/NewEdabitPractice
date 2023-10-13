35c2f5259a21f11d2ad6cc1294d2bc9f60cabf8f
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-10-13 00:04:04 -0500

    finalizing stuff for next time

--- easy/src/app/easy/easy.component.ts
+++ easy/src/app/easy/easy.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-easy',
   templateUrl: './easy.component.html',
-  styleUrls: ['./easy.component.css']
+  styleUrls: ['./easy.component.css'],
 })
 export class EasyComponent {
-
   isCardVisible = false;
 
   toggleCardVisibility() {
     this.isCardVisible = !this.isCardVisible;
   }
-
-
 }
